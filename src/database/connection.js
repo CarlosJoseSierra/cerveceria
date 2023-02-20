8e605bf3757f71c7a18b7463879a85c7ef4772bd
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2023-02-20 11:07:44 -0500

    second commit cerveceria

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,10 +2,15 @@ import sql from "mssql";
 import config from "../config";
 
 export const dbSettings = {
-  user: config.dbUser,
+ /*  user: config.dbUser,
   password: config.dbPassword,
   server: config.dbServer,
-  database: config.dbDatabase, 
+  database: config.dbDatabase, */
+  user: "sa",
+  password: "Solomon2011",
+  server: "181.198.20.179",
+  //server: "DESKTOP-DQQ2201",
+  database: "DB_PRODUCCION",
   options: {
     encrypt: false, // for azure
     trustServerCertificate: true, // change to true for local dev / self-signed certs
