cad6407c1259b9f53373f76bd8c9b44f55a63d36
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2023-07-30 20:50:24 -0500

    crev2023-c3

--- src/controllers/activos.controller.js
+++ src/controllers/activos.controller.js
@@ -25,7 +25,8 @@ export const getcountActivo = async (req, res) => {
 export const createNewActivo = async (req, res) => {
   const { EQC_serie, EQC_placa, EQC_EQUIP_id,EQC_MARCA_id,EQC_MAP_ciudad,EQC_MAP_provincia,EQC_MAP_address,EQC_USU_ing,
     EQC_codTag,EQC_LOGO_id,EQC_nombreCliente,EQC_identificacionCliente,EQC_direccionCliente,EQC_NegocioCliente,
-    EQC_telefonoCliente,EQC_cambio,EQC_estadoEquipo,EQC_observacion, EQC_TI_id,EQC_USU_edit} = req.body;
+    EQC_telefonoCliente,EQC_cambio,EQC_estadoEquipo,EQC_observacion,EQC_TI_id,EQC_USU_edit,EQC_UBIC_id,
+    EQC_provincia,EQC_cabezales} = req.body;
   
   // validating
   if (EQC_serie == null || EQC_placa == null ||  EQC_EQUIP_id==null || EQC_USU_ing == null || EQC_codTag == null || EQC_LOGO_id == null) {
@@ -56,7 +57,10 @@ export const createNewActivo = async (req, res) => {
       .input("EQC_estadoEquipo",sql.Decimal,EQC_estadoEquipo)
       .input("EQC_observacion",sql.VarChar,EQC_observacion)
       .input("EQC_TI_id", sql.Decimal, EQC_TI_id)
-      
+      .input("EQC_UBIC_id",sql.Decimal,EQC_UBIC_id)
+      .input("EQC_provincia",sql.VarChar,EQC_provincia)
+      .input("EQC_cabezales", sql.Decimal, EQC_cabezales)
+
       .query(querys.addNewActivo);
       if(result.rowsAffected==1){
         return res.status(200).json({ status: "ok", msg: "Registro exitoso" ,token:0});
@@ -74,7 +78,8 @@ export const createNewActivo = async (req, res) => {
 export const updateActivoById = async (req, res) => {
   const { EQC_serie, EQC_placa, EQC_EQUIP_id,EQC_MARCA_id,EQC_MAP_ciudad,EQC_MAP_provincia,EQC_MAP_address,EQC_USU_ing,
     EQC_codTag,EQC_LOGO_id,EQC_nombreCliente,EQC_identificacionCliente,EQC_direccionCliente,EQC_NegocioCliente,EQC_telefonoCliente,
-    EQC_cambio,EQC_estadoEquipo,EQC_observacion,EQC_TI_id,EQC_USU_edit} = req.body;
+    EQC_cambio,EQC_estadoEquipo,EQC_observacion,EQC_TI_id,EQC_USU_edit,EQC_UBIC_id,
+    EQC_provincia,EQC_cabezales} = req.body;
 
   // validating
   if (EQC_serie == null || EQC_placa == null ||  EQC_EQUIP_id==null  || EQC_USU_edit == null || EQC_codTag == null || EQC_LOGO_id == null) {
@@ -107,6 +112,9 @@ export const updateActivoById = async (req, res) => {
       .input("EQC_estadoEquipo",sql.Decimal,EQC_estadoEquipo)
       .input("EQC_observacion",sql.VarChar,EQC_observacion)
       .input("EQC_TI_id", sql.Decimal, EQC_TI_id)
+      .input("EQC_UBIC_id",sql.Decimal,EQC_UBIC_id)
+      .input("EQC_provincia",sql.VarChar,EQC_provincia)
+      .input("EQC_cabezales", sql.Decimal, EQC_cabezales)
       .query(querys.updateActivoById);
 
    if(result.rowsAffected==1){
